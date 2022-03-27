1a99abf3115f0497d74174777b332330546e1f5a
hans <hans.bourgeois@ulb.be>
2022-03-27 15:39:15 +0200

    27-03-22 | test valgrind

--- exec_command.c
+++ exec_command.c
@@ -6,7 +6,7 @@
 /*   By: hbourgeo <hbourgeo@student.19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 16:08:07 by hbourgeo          #+#    #+#             */
-/*   Updated: 2022/03/27 13:39:35 by hbourgeo         ###   ########.fr       */
+/*   Updated: 2022/03/27 15:38:10 by hbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,19 +29,12 @@ void	ft_free_array(char **array)
 	free(array);
 }
 
-char	**getparam(char *cmd_arg)
+char	**get_cmd_param(char *cmd_arg)
 {
-	char	**param;
-	size_t	len;
-
-	len = 0;
-	while (*cmd_arg && *cmd_arg != ' ')
-	{
-		cmd_arg++;
-	}
-	cmd_arg++;
-	param = ft_split(cmd_arg, ' '); /*free ? [N]*/
-	return (param);
+	char	**cmd_param;
+	
+	cmd_param = ft_split(cmd_arg, ' '); /*free ? [N]*/
+	return (cmd_param);
 }
 
 char	*my_getenv(char *name, char **env)
@@ -60,63 +53,56 @@ char	*my_getenv(char *name, char **env)
 	return (NULL);
 }
 
-char	*getcmd(char *cmd_arg)
-{
-	char	*cmd;
-	size_t	len;
-
-	len = 0;
-	while (cmd_arg[len] && cmd_arg[len] != ' ')
-	{
-		len++;
-	}
-	cmd = ft_calloc(len + 1, sizeof(char));
-	if (!cmd)
-		return (NULL);
-	ft_strlcpy(cmd, cmd_arg, len + 1); /*free [Y]*/
-	return (cmd);
-}
-
 char	*getpath(char *cmd_arg, char **env)
 {
 	char	**all_paths;
-	char	*cmd;
+	char	**cmd_param;
 	char	*folder_path;
 	char	*cmd_path;
 	size_t 	i;
 
 	i = 0;
 	all_paths = ft_split(my_getenv("PATH", env), ':');
-	cmd = getcmd(cmd_arg);
+	cmd_param = get_cmd_param(cmd_arg);
 	while (all_paths[i])
 	{
 		folder_path = ft_strjoin(all_paths[i], "/"); /*free [Y]*/
-		cmd_path = ft_strjoin(folder_path, cmd); /*free [Y]*/
+		cmd_path = ft_strjoin(folder_path, cmd_param[0]); /*free [Y]*/
 		free(folder_path);
 		if (access(cmd_path, F_OK | X_OK) == 0) /*voir errno*/
 		{
-			free(cmd);
+			ft_free_array(cmd_param);
 			ft_free_array(all_paths);
 			return (cmd_path);
 		}
 		free(cmd_path);
 		i++;
 	}
-	free(cmd);
+	ft_free_array(cmd_param);
 	ft_free_array(all_paths);
 	return (NULL);
 }
 
 int main(int argc, char **argv, char **envp)
 {
-	char	**param;
+	char	**cmd_param;
 	char	*path;
+	size_t	n;
 
-	param = getparam(argv[1]);
+	n = 0;
+
+	// printf("%s\n", *argv + 1);
+	cmd_param = get_cmd_param(argv[1]);
+	
 	path = getpath(argv[1], envp);
+	printf("%s\n", path);
 	if (argc > 1)
-		if (execve(path, param, envp) == -1)
+	{
+		if (execve(path, cmd_param, NULL) == -1)
 			perror("execve");
-	ft_free_array(param);
-	printf("My pid is: %d\n", getpid());
+	}
+	ft_free_array(cmd_param);
+	free(path);
+	printf("My pid is: %s\n", "test");
+	return (0);
 }
